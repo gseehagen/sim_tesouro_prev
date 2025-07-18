bf3e420db03787c49230913ebaf3b2b8a099f75a
Guilherme Augusto Seehagen <guilherme.seehagen@unioeste.br>
2025-07-18 18:02:16 -0300

    second commit

--- src/main.py
+++ src/main.py
@@ -1,18 +1,21 @@
 import pandas as pd
-from models.titulo import Titulo  
+from models.titulo import Titulo
 
 
 df = pd.read_csv(r'C:\Users\guilherme.seehagen\OneDrive - UNIOESTE\Área de Trabalho\mq3_trabalho\data\tesouro_direto_titulos.csv')
 
-titulos = []
+# Converte a rentabilidade para decimal (7.62 → 0.0762)
+df['Rentabilidade anual'] = df['Rentabilidade anual'].astype(float) / 100
 
-for _, row in df.iterrows():
-    titulo = Titulo(
+# Cria os títulos
+titulos = [
+    Titulo(
         nome=row['Título'],
         vencimento=row['Vencimento'],
         taxa_juros_anual=row['Rentabilidade anual']
     )
-    titulos.append(titulo)
+    for _, row in df.iterrows()
+]
 
-#Teste do titulo
-print(titulos[0].resumo_do_titulo())
+# Teste
+titulos[2].resumo_do_titulo()
